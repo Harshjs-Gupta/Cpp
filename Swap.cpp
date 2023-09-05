37b6dbb8cfb5e246982cfb615c69e54f18492887
Harsh Gupta <harshgupta88911@gmail.com>
2023-09-05 11:47:57 +0530

    New file is created

---
+++ Swap.cpp
@@ -0,0 +1,33 @@
+// write a program to swap the value
+//  #include <iostream>
+//  using namespace std;
+
+// void swap(int x, int y) {
+//     int temp = x;
+//     x = y;
+//     y = temp;
+// }
+
+// int main() {
+//     int a = 10, b = 20;
+//     swap(a, b);
+//     cout << "a = " << a << ", b = " << b;
+//     return 0;
+// }
+
+
+//write a program to swap the value without taking third variable;
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a, b;
+//     std::cout << "Enter the number: ";
+//     cin >> a >> b;
+//     a = a + b; // 2 + 3 =5
+//     b = a - b; // 5 -3 = 2
+//     a = a - b; // 5 - 2 = 3
+//     std::cout << "Swap value is: " << a << " " << b << endl;
+
+//     return 0;
+// }
