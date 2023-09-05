9d4f951b63757afc937ad5e16b89e52b5d950f08
Harsh Gupta <harshgupta88911@gmail.com>
2023-09-05 11:45:52 +0530

    New file is created

---
+++ EvenOrOdd.cpp
@@ -0,0 +1,49 @@
+// write a program to check the number is even or odd
+// #include <iostream>
+// using namespace std;
+
+// string checkEvenOdd(int number)
+// {
+//     if (number % 2 == 0)
+//     {
+//         return "even";
+//     }
+//     else
+//     {
+//         return "odd";
+//     }
+// }
+// int main()
+// {
+//     int num;
+//     std::cout << "Enter the number: ";
+//     std::cin >> num;
+
+//     checkEvenOdd(num);
+
+//     std::cout << "Number is: " << checkEvenOdd(num) << endl;
+
+//     return 0;
+// }
+
+
+
+/*write a program to print the word when (number <= 9) then print like this "one","two","three". and 
+when (number > 9) then print even or odd according to number*/
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int num;
+//   cout<<"Enter the number: ";
+//   cin>>num;
+//   string word={"one","two","three","four","five","six","seven","eight","nine"};
+   
+//   if(1<= num <=9)
+//   cout<<word[num-1];
+//   else 
+//   cout<<"greater then 9";
+
+//     return 0;
+// }
