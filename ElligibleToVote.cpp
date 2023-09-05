7646b583201eeae53d9734559578c7311752aeda
Harsh Gupta <harshgupta88911@gmail.com>
2023-09-05 11:43:44 +0530

    New file is created

---
+++ ElligibleToVote.cpp
@@ -0,0 +1,25 @@
+/*A person is elligible to vote if his/her age is greater than or equal to 18.
+efine a function to find out if he/she is elligible to vote*/
+// #include <iostream>
+// using namespace std;
+// string checkage(int age)
+// {
+//     if (age >= 18)
+//     {
+//         return "is elligible to vote";
+//     }
+//     else
+//     {
+//         return "is not elligible to vote";
+//     }
+// }
+// int main()
+// {
+//     int num;
+//     std::cin >> num;
+
+//     checkage(num);
+
+//     std::cout << "Number is: " << checkage(num) << endl;
+//     return 0;
+// }
