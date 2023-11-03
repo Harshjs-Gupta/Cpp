cff5931e072619fface04717b0b7fef7543dd140
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-03 16:43:34 +0530

    New file is created

---
+++ Function/CheckGrade.cpp
@@ -0,0 +1,37 @@
+/*Write a program which will ask the user to enter his/her marks (out of 100)
+Define a function that will display grades according to the marks entered as below:
+Marks        Grade
+91-100         AA
+81-90          AB
+71-80          BB
+61-70          BC
+51-60          CD
+41-50          DD
+<=40          Fail*/
+// #include<iostream>
+// using namespace std;
+// int checkgrade(int marks)
+// {
+//     if(marks>=0 && marks<=40)
+//     cout<<marks<<" your grade are E";
+//     else if(marks>=41 && marks<=50)
+//     cout<<marks<<" your grade are D";
+//     else if(marks>=51 && marks<=60)
+//     cout<<marks<<" your grade are C";
+//     else if(marks>=61 && marks<=70)
+//     cout<<marks<<" your grade are B";
+//     else if(marks>=71 && marks<=80)
+//     cout<<marks<<" your grade are B+";
+//     else if(marks>=81 && marks<=90)
+//     cout<<marks<<" your grade are A";
+//     else if(marks>=91 && marks<=100)
+//     cout<<marks<<" your grade are A+";
+// }
+// int main()
+// {
+//     int num;
+//     cout<<"Enter the number: ";
+//     cin>>num;
+//     checkgrade(num);
+//     return 0;
+// }
