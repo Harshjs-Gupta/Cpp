f0a475ed6f963523785752903e9a37473bddf7a5
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-03 16:39:29 +0530

    New file is created

---
+++ Function/Additon.cpp
@@ -0,0 +1,16 @@
+// Write a program to print the sum of two numbers entered by user by defining your own function
+//  #include<iostream>
+//  using namespace std;
+
+// int add(int a,int b)
+// {
+//     int c;
+//     c=a+b;
+//     return c;
+// }
+// int main()
+// {
+//     int x=5,y=3,z;
+//     z=add(x,y);
+//     cout<<"sum is: "<<z<<endl;
+// }
