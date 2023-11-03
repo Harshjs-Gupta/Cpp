367899e90ead58764c8ae78f6ccca2efe794e38e
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-03 16:42:16 +0530

    New fiel is created

---
+++ Function/Areaofcircle.cpp
@@ -0,0 +1,17 @@
+/*Write a program to print the circumference and area of a circle of radius entered by
+user by defining your own function*/
+// #include<iostream>
+// using namespace std;
+
+// float circle(float r)
+// {
+//     return "circumference is: "<<2 * 3.14 * r<<"\n";
+//     cout<<"area is: "<<3.14 * r * r<<"\n";
+// }
+// int main()
+// {
+//     int r;
+//     cout<<"Enter the radius: ";
+//     cin>>r;
+//     circle(r);
+// }
