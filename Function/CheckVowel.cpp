11971462711ab276ae533493fb9ba9e75d489e49
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-07 12:00:42 +0530

    New file is created

---
+++ Function/CheckVowel.cpp
@@ -0,0 +1,21 @@
+// write a program when a user can enter character and check if it is vowel or not
+// #include <iostream>
+// using namespace std;
+
+// bool checkVowel(char ch)
+// {
+//     return (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+// }
+
+// int main()
+// {
+//     char ch;
+//     bool isVowel;
+//     cout << "Enter the character: ";
+//     cin >> ch;
+//     isVowel = checkVowel(toupper(ch));
+
+//     cout << boolalpha << isVowel;
+
+//     return 0;
+// }
