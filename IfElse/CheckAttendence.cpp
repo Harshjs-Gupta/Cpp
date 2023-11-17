ef7ba5664add2259dc81776145b6b243e4221fef
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 13:42:56 +0530

    New file is created

---
+++ IfElse/CheckAttendence.cpp
@@ -0,0 +1,25 @@
+/*write a program to A student will not be allowed to sit in exam if his/her
+attendence is less than 75%.
+Take following input from user
+Number of classes held
+Number of classes attended.
+And print
+percentage of class attended
+Is student is allowed to sit in exam or not.*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int percentage;
+//    cout<<"Enter the percentage: ";
+//    cin>>percentage;
+//    if(percentage >=75)
+//    {
+//     cout<<"student are allowed to sit in exam";
+//    }
+//    else
+//    {
+//     cout<<"student are not allowed";
+//    }
+//     return 0;
+// }
