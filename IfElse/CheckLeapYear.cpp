db9de590a6b71700abea2ae44124a994484b820f
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 13:44:54 +0530

    New file is created

---
+++ IfElse/CheckLeapYear.cpp
@@ -0,0 +1,22 @@
+//write a program to check is a year is leap year or not
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int year;
+//    cout<<"Enter the year: ";
+//    cin>>year;
+//    if(year % 400==0)
+//    {
+//     cout<<year<<" year is a leap year";
+//    }
+//    else if(year % 100 ==0)
+//    {
+//     cout<<year<<" year is not a leap year";
+//    }
+//    else if(year % 4 ==0)
+//    {
+//     cout<<year<<" year is a leap year";
+//    }
+//     return 0;
+// }
