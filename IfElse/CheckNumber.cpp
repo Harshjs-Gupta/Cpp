aac9edbd816207fc8a1882981daafc93b5c8df45
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 13:47:19 +0530

    New file is created

---
+++ IfElse/CheckNumber.cpp
@@ -0,0 +1,43 @@
+/*write a program to Take 20 integer inputs from user and print the following:
+number of positive numbers
+number of negative numbers
+number of odd numbers
+number of even numbers
+number of 0*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int array[20]={0,2,3,4,8,9,10,0,-13,-55,-50,-82,45,71,82,3,7,-18,19};
+//     int Positive=0,Negative=0,Odd=0,Even=0,Zero=0;
+//     for(int i=0;i<20;i++)
+//     {
+//         if(array[i] > 0)
+//         {
+//             Positive++;            
+//         }
+//         if(array[i] < 0)
+//         {
+//            Negative++;
+//         }
+//         if(array[i] % 2 == 0)
+//         {
+//            Even++;
+//         }
+//         if(array[i] % 2 != 0)
+//         {
+//             Odd++;
+//         }
+//         else
+//         {
+//             Zero++;
+//         }
+//     }
+//      cout<<"Positive number"<< Positive<<"\n";
+//      cout<<"Negative number "<<Negative<<"\n";
+//      cout<<"Even number"<< Even<<"\n";
+//      cout<<"Odd  number"<< Odd<<"\n";
+//      cout<<"Zero number"<< Zero<<"\n";
+
+//     return 0;
+// }
