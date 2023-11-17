93b17532fc86ab1bc319bf28674c7ff82e912781
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 14:08:47 +0530

    New file is created

---
+++ IfElse/GiveDiscount.cpp
@@ -0,0 +1,23 @@
+/*write a program to A shop will give discount of 10% if the cost of purchased
+ quantity is more then 1000. 
+ Ask user for quantity 
+ Suppose, one unit will cost 100
+ Judge and print total */
+//  #include<iostream>
+//  using namespace std;
+//  int main()
+//  {
+//     int price,quantity;
+//     cout<<"Enter the quantity: ";
+//     cin>>quantity;
+//     price=quantity*100;
+//     if(price>1000)
+//     {
+//         cout<<"Total cost is: "<<price-(price*.1)<<endl;
+//     }
+//     else
+//     {
+//         cout<<"Total cost is: "<<price<<endl;
+//     }
+//     return 0;
+//  }
