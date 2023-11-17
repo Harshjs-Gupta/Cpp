feb478eed98972533202e234dfc3db2cca76a7ae
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 14:03:55 +0530

    New file is created

---
+++ IfElse/FindNumber.cpp
@@ -0,0 +1,28 @@
+/*write a program to Take 10 integer inputs from user and store them in an array. 
+Again ask user to give a number.Now, tell user whether that number is present in array or not*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int array[10]={1,2,3,4,5,6,7,8,9,10};
+//    int n,found=0;
+//    cout<<"Enter the number: ";
+//    cin>>n;
+//    for(int i=0;i<array[i];i++)
+//    {
+//     if(array[i]==n)
+//     {
+//         cout<<"found"<<endl;
+//         exit;
+//     }
+//    }
+//    if(found)
+//    {
+//     cout<<"number is present";
+//    }
+//    else
+//    {
+//     cout<<"number is not present";
+//    }
+//     return 0;
+// }
