28cd5d3b192cb178df36830fd98597a0cbe09652
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 13:56:41 +0530

    New file is created

---
+++ IfElse/CheckSquareOrNot.cpp
@@ -0,0 +1,21 @@
+/*write a program to take value of length and breadth of a rectangle from user and 
+check if it is square or not*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int l,b;
+//    cout<<"Enter the length: ";
+//    cin>>l;
+//    cout<<"Enter the breadth: ";
+//    cin>>b;
+//    if(l == b)
+//    {
+//     cout<<"It is a square";
+//    }
+//    else
+//    {
+//     cout<<"It is a rectangle";
+//    }
+//     return 0;
+// }
