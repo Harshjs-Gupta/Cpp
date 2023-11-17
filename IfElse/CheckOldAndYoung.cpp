4d610fc8e5b115f5194353a8daffa2dfefd53e95
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-17 13:52:49 +0530

    New file is created

---
+++ IfElse/CheckOldAndYoung.cpp
@@ -0,0 +1,41 @@
+/*write a program to take the input of age of 3 people by user and determine
+ oldest and youngest among them */
+//  #include<iostream>
+//  using namespace std;
+//  int main()
+//  {
+//     int x,y,z,age;
+//     cout<<"Enter the age: ";
+//     cin>>x>>y>>z;
+//     if((x>y) && (x>z))
+//     {
+//         cout<<x<<" is oldest";
+//         cout<<endl;
+//     }
+//     else
+//     {
+//         cout<<x<<" is youngest";
+//         cout<<endl;
+//     }
+//     if((y>x) && (y>z))
+//     {
+//         cout<<y<<" is oldest";
+//         cout<<endl;
+//     }
+//     else
+//     {
+//         cout<<y<<" is youngest";
+//         cout<<endl;
+//     }
+//     if((z>x) && (z>y))
+//     {
+//         cout<<z<<" is oldest";
+//         cout<<endl;
+//     }
+//     else
+//     {
+//         cout<<z<<" is youngest";
+//         cout<<endl;
+//     }
+//     return 0;
+//  }
