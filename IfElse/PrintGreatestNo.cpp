376fc9ea09c9a55a43e7c299ee971cfc4eec7c45
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-18 12:01:05 +0530

    New file is created

---
+++ IfElse/PrintGreatestNo.cpp
@@ -0,0 +1,18 @@
+//write a program to take two int values from user and print greatest among them 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y;
+//     cout<<"Enter the number: ";
+//     cin>>x>>y;
+//     if(x > y)
+//     {
+//         cout<<x<<" is greater then y";
+//     }
+//     else
+//     {
+//         cout<<y<<" is greater then x";
+//     }
+//     return 0;
+// }
