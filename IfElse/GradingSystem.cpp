e3d3ce0f4a617ff4b7e0da8f4263c83ecf780d4c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-18 11:59:47 +0530

    New file is created

---
+++ IfElse/GradingSystem.cpp
@@ -0,0 +1,49 @@
+/*write a program to A school has following rules for grading system:
+a. Below 25 - F
+b. 25 to 45 - E
+c. 45 to 50 - D
+d. 50 to 60 - C
+e. 60 to 80 - B
+f. Above 80 - A
+Ask user to enter marks and print the corresponding grade.*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int marks;
+//     cout<<"Enter the marks: ";
+//     cin>>marks;
+//     if(marks <= 25)
+//     {
+//         cout<<"F grade";
+//     }
+//     else if(marks >25 && marks <=40)
+//     {
+//         cout<<"E grade";
+//     }
+//     else if(marks >40 && marks <=50)
+//     {
+//         cout<<"D grade";
+//     }
+//     else if(marks >50 && marks <=60)
+//     {
+//         cout<<"C grade";
+//     }
+//     else if(marks >60 && marks <=80)
+//     {
+//         cout<<"B grade";
+//     }
+//     else if(marks >80 && marks <=90)
+//     {
+//         cout<<"A grade";
+//     }
+//     else if(marks >90 && marks <=100)
+//     {
+//         cout<<"A++ grade";
+//     }
+//     else
+//     {
+//         cout<<"Wrong marks";
+//     }
+//     return 0;
+// }
