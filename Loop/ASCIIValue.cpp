1a34b8a5f7d453d05b5d245fa40962e912ac12d4
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-22 12:31:07 +0530

    New file is created

---
+++ Loop/ASCIIValue.cpp
@@ -0,0 +1,14 @@
+/*write a program to Print ASCII values and their equivalent characters.
+ ASCII value vary from 0 to 255.*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char c;
+//     for(int i=0;i<=255;i++)
+//     {
+//         c = i;
+//         cout<<c<<"\n";
+//     }
+//     return 0;
+// }
