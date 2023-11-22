b351024f5506ba60609fa818985dc7c7acdb2b0c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-22 12:32:52 +0530

    New file is created

---
+++ Loop/AverageValue.cpp
@@ -0,0 +1,16 @@
+/*write a program to Take 10 integers from keyboard using loop and print their 
+average value on the screen.*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float n,sum=0;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i=0;i<=n;i++)
+//     {
+//         sum=sum+i;
+//     }
+//     cout<<"The average value is: "<<sum/10.0<<"\n";
+//     return 0;
+// }
