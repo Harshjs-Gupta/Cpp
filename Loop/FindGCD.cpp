4303e2f91851a06094177a2d39885e9ef03b03e9
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-22 12:35:26 +0530

    New filei screated

---
+++ Loop/FindGCD.cpp
@@ -0,0 +1,22 @@
+// Write a program in C++ to find the Greatest Common Divisor (GCD) of two numbers.
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num1, num2, gcd;
+//     cout << "Enter the num1: ";
+//     cin >> num1;
+//     cout << "Enter the num2: ";
+//     cin >> num2;
+//     for (int i = 1; i <= num1 && i <= num2; i++)
+//     {
+//         if (num1 % i == 0 && num2 % i == 0)
+//         {
+//             gcd = i;
+//         }
+//     }
+//     cout << "The greatest common divisor is: " << gcd << endl;
+
+//     return 0;
+// }
