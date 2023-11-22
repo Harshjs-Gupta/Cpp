6db9244826228be754471bdc8c79df7563896768
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-22 12:37:09 +0530

    New file s created

---
+++ Loop/Findtheprime.cpp
@@ -0,0 +1,26 @@
+// Write a program in C++ to find a prime number within a range
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main()
+// {
+//    int n,count=0,find=0;
+//    cout<<"Enter the number: ";
+//    cin>>n;
+//    for(int i=1;i<=n;i++)
+//    {
+//         for(int j=2;j<=sqrt(i);j++)
+//                {
+//                if(i%j==0)
+//                   count++;
+//                }
+//                if(count==0 && i!=1)
+//                { 
+//                 find++;
+//                 cout<<i<<" "; 
+//                }
+//                count=0;
+//    }
+
+//     return 0;
+// }
