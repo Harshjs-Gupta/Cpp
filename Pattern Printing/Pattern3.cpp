f93cd2016c4be3ddcc44c00200f20eb1d402372c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-23 15:36:11 +0530

    Pattern printing method 3

---
+++ Pattern Printing/Pattern3.cpp
@@ -0,0 +1,47 @@
+//write a program to make the pattern 3
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=0;i<9;i++)
+//     {
+//         for(int j=0;j<9;j++)
+//         {
+//             if(i+j>10-1)
+//             {
+//                 cout<<"*";
+//             }
+//             else
+//             {
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=0;i<9;i++)
+//     {
+//         for(int j=0;j<9;j++)
+//         {
+//             if(i<j)
+//             {
+//                 cout<<"*";
+//             }
+//             else
+//             {
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
