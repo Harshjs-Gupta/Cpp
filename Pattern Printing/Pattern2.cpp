6185612eca3a8a959ddd866a68b1f2970d3c42a6
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-23 15:35:09 +0530

    Pattern Printing method 2

---
+++ Pattern Printing/Pattern2.cpp
@@ -0,0 +1,100 @@
+//write a program to make the pattern 2
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=0;i<9;i++)
+//     {
+//         for(int j=0;j<9;j++)
+//         {
+//             if(i<j)
+//             {
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+//write a program to print the triangle using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(int i=5;i>=1;i--)
+//    {
+//     for(int j=1;j<=i;j++)
+//     {
+//             cout<<i;
+//     }
+//     cout<<"\n";
+//    }
+
+//     return 0;
+// }
+
+
+
+//write a program to print the triangle using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(int i=1;i<=5;i++)
+//    {
+//     for(int j=5;j>=i;j--)
+//     {
+//         cout<<j;
+//     }
+//     cout<<"\n";
+//    }
+
+
+
+//     return 0;
+// }
+
+
+
+//write a program to print teh triangle using alphabets
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(char i=69;i>=65;i--)
+//     {
+//         for(char j=65;j<=i;j++)
+//         {
+//                 cout<<i;
+//         }
+//         cout<<"\n";
+//     }
+
+
+
+//         return 0;
+// }
+
+
+//write a program to print the triangle using albhabets
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(char i=65;i<=69;i++)
+//    {
+//         for(char j=69;j>=i;j--)
+//         {
+//                 cout<<j;
+//         }
+//         cout<<"\n";
+//    }
+
+//         return 0;
+// }
+
+
+
