2466fbcb53481fe49b85212ad3f1113aaff84086
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-23 15:32:55 +0530

    Pattern Printing method 1

---
+++ Pattern Printing/Pattern1.cpp
@@ -0,0 +1,64 @@
+//write a program to make right angle triangle
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=0;i<9;i++)
+//     {
+//         for(int j=0;j<9;j++)
+//         {
+//             if(i>j)
+//             {
+//                 cout<<"*";
+//             }
+//         } 
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+//write a program to print the triangle using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(int i=0;i<=5;i++)
+//    {
+//     for(int j=0;j<=5;j++)
+//     {
+//         if(i>j)
+//         {
+//             cout<<i;
+//         }
+//     }
+//     cout<<endl;
+//    }
+
+
+
+//     return 0;
+// }
+
+
+//write a program to print the triangle using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(int i=1;i<=5;i++)
+//    {
+//     for(int j=1;j<=5;j++)
+//     {
+//         if(i>j)
+//         {
+//             cout<<j;
+//         }
+//     }
+//     cout<<"\n";
+//    }
+
+//     return 0;
+// }
+
+
