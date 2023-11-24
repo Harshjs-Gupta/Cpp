270b9273fdee366d3f167ae98502cc4175189afd
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-24 13:53:38 +0530

    Pattern Printing Method 5

---
+++ Pattern Printing/pattern5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int count=0,c=0;
+for(int i=0;i<10;i++)
+{
+     cout<<"\n";
+     for(int j=1;j<5;j*=2)
+     {
+        cout<<"\n word";
+        c++; 
+     }
+     count++;
+}
+
+   return 0;
+}
+
