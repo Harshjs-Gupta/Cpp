27aba787fa07513d3a47331960c7b592d236b68d
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-24 14:01:22 +0530

    Pattern Printing Method 7

---
+++ Pattern Printing/Pattern7.cpp
@@ -0,0 +1,72 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    for(int i=0;i<=5;i++)
+//    {
+//     for(int j=0;j<5;j++)
+//     {
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+//    }
+
+
+//     return 0;
+// }
+
+
+//write a program to print the square using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=1;i<=5;i++)
+//     {
+//         for(int j=1;j<=5;j++)
+//         {
+//             cout<<i;
+//         }
+//         cout<<"\n";
+//     }
+ 
+//     return 0;
+// }
+
+
+//write a program to print the square using number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=1;i<=5;i++)
+//     {
+//         for(int j=1;j<=5;j++)
+//         {
+//             cout<<j;
+//         }
+//         cout<<"\n";
+//     }
+
+
+//     return 0;
+// }
+
+
+//write a progam to print the square using number
+#include<iostream>
+using namespace std;
+int main()
+{
+     for(char i='A';i<='E';i++)
+     {
+        for(char j='A';j<='E';j++)
+        {
+            cout<<i;
+        }
+        cout<<"\n";
+     }
+
+     
+    return 0;
+}
