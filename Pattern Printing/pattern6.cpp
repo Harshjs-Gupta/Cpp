3f8fd9bd1176a456cf3d109bd88ba066f58ca85b
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-24 13:57:05 +0530

    Patterb Printing Method 6

---
+++ Pattern Printing/pattern6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0,c=0;
+    for(int i=0;i<5;i++)
+    {
+        cout<<"\n";
+        for(int j=0;j<5;j++)
+        {
+            cout<<"\n Hello";
+            c++;
+        }
+        count++;
+    }
+
+    return 0;
+}
