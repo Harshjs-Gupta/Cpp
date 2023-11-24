e0ffe2e64a4c98790d7ea64cacfc6016acaecedd
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-24 13:49:40 +0530

    Pattern Printing Method 4

---
+++ Pattern Printing/pattern4.cpp
@@ -0,0 +1,26 @@
+//write  program to print the opposite right hand-side triangle printing
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=0;i<5;i++)
+//     {
+//         for(int j=0;j<5;j++)
+//         {
+//             if(i>j)
+//             {
+//                 cout<<" ";
+//             }
+//             else
+//             {
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+//     }
+
+//     return 0;
+
+// }
+
+
