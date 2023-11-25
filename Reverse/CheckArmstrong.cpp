8e16325a1033cd0b310d395c06f54c19d5703ad9
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-25 13:27:29 +0530

    Check Armstrong

---
+++ Reverse/CheckArmstrong.cpp
@@ -0,0 +1,25 @@
+//write a program to check the armstrong 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int m,n,r,sum=0;
+//   cout<<"Enter the number: ";
+//   cin>>n;
+//   m=n;
+//   while(n>0)
+//   {
+//     r=n%10;
+//     n=n/10;
+//     sum=sum+(r*r*r);
+//   }
+//   if(m==sum)
+//   {
+//     cout<<"Its an armstrong number";
+//   }
+//   else
+//   {
+//     cout<<"Its not an armstrong number";
+//   }  
+//     return 0;
+// }
