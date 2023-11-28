807657c176c62d764441472bf7398bf7767daacd
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-28 14:14:49 +0530

    Check String

---
+++ String/CheckString.cpp
@@ -0,0 +1,17 @@
+//write a program to check the string is empty or not 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str=" ";
+   if(str.empty())
+   {
+    cout<<"string is empty"<<endl;
+   }
+   else
+   {
+    cout<<"string is "<<str<<endl;
+   }
+return 0;
+}
