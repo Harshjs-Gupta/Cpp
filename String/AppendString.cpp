01bc4aa22127843ad75c0ab9f3311e344289106c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-28 14:07:45 +0530

    Append string

---
+++ String/AppendString.cpp
@@ -0,0 +1,11 @@
+//write a program to append the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str="Hello ";
+    str.append("World");
+    cout<<str<<endl;
+return 0;
+}
