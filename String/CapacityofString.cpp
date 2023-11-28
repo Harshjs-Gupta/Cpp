8fa2a575c586af0a058fd8eee1b92e7b9e020bf5
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-28 14:10:44 +0530

    Check capacity of string

---
+++ String/CapacityofString.cpp
@@ -0,0 +1,11 @@
+//write a program to check the capacity of string 
+// #include<iostream>
+// #include<string.h>
+// using namespace std;
+// int main()
+// {
+//    string str="Hello";
+//    cout<<str.capacity();
+//    cout<<endl;
+// return 0;
+// }
