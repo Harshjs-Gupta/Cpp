adfae5dbaa7e334c20976355104cdf55d33515ad
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-30 01:05:22 +0530

    Display string

---
+++ String/DisplaySentence.cpp
@@ -0,0 +1,12 @@
+//Write a program to input and display the sentence 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout<<"Enter the sentance: ";
+    cin.getline(str,20);
+    cout<<str<<endl;
+return 0;
+}
