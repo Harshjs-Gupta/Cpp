41610f543e5c8b8e8327570acc0513f813f5c44a
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-29 14:56:29 +0530

    compare String

---
+++ String/CompareString.cpp
@@ -0,0 +1,11 @@
+//write a program to compare the string
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str1="Hello";
+   string str2="Hello";
+   cout<<str1.compare(str2)<<endl;
+return 0;
+}
