fe21168b2ac3314682f942f31ddd143039bf0a7c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-30 01:01:42 +0530

    Concatenation of string

---
+++ String/Concatenation.cpp
@@ -0,0 +1,12 @@
+//write a program to concatenate the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+  char str1[20]="Programming ";
+  char str2[20]="Language";
+  strcat(str1,str2);
+  cout<<str1<<endl;
+return 0;
+}
