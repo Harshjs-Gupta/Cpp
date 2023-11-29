530683d1695d3ea5816d7596a3d682f813533240
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-30 01:06:46 +0530

    Find letters

---
+++ String/FindLetter.cpp
@@ -0,0 +1,10 @@
+//write a program to find the letter from string
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str="Hello World";
+   cout<<str.find('l')<<endl;
+return 0;
+}
