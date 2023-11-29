7a81141384e86b6a4284ba223550765a8c2ccb95
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-29 14:23:59 +0530

    Clear String

---
+++ String/ClearString.cpp
@@ -0,0 +1,11 @@
+//write a program to clear the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+  string str="Hello world";
+  str.clear();
+  cout<<str<<endl;
+return 0;
+}
