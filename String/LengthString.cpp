cfd77e848d8965a48a0b40e3a88b4a37622ec201
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-01 12:56:25 +0530

    Length String

---
+++ String/LengthString.cpp
@@ -0,0 +1,11 @@
+//write a program to print the length of string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str="Hello";
+    cout<<str.length();
+    cout<<endl;
+    return 0;
+}
