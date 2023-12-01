c85e0db8ddd6aca0df609ebf68ddf656acc3e909
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-01 12:53:37 +0530

    Insert String

---
+++ String/InsertString.cpp
@@ -0,0 +1,11 @@
+//write a program to insert the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str="Hello Wrld";
+    str.insert(7,"o");
+    cout<<str<<endl;
+return 0;
+}
