7f64dc72e855345fa5e41961a4921de964b5bb3a
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-01 12:58:15 +0530

    Pop Back String

---
+++ String/PopBackString.cpp
@@ -0,0 +1,11 @@
+//write a program to pop back the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str="Hello Worlde";
+   str.pop_back();
+   cout<<str<<endl;
+return 0;
+}
