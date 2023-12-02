50456c22e98e15c48365cb200e96ef246ab3dd77
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-02 14:01:35 +0530

    Push Back String

---
+++ String/PushBackString.cpp
@@ -0,0 +1,11 @@
+//write a program to push back the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str="Hello Worl";
+    str.push_back('d');
+    cout<<str<<endl;
+return 0;
+}
