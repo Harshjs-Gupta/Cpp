ddfa7aaf7d54ca3981a2ae0d1d58622b90b2f0df
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-02 14:02:36 +0530

    Replace String

---
+++ String/ReplaceString.cpp
@@ -0,0 +1,11 @@
+//write a program to replace the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str="Hello Wouhd";
+   str.replace(8,2,"rl");
+   cout<<str<<endl;
+return 0;
+}
