ec81ba98afa1e290f2cd8fdeeb10db2e3ad1fe79
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-02 13:58:36 +0530

    Print Sub String

---
+++ String/PrintSubString.cpp
@@ -0,0 +1,10 @@
+//write a program to print the substring 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+  string str="Programming";
+  cout<<str.substr(3,4)<<endl;
+return 0;
+}
