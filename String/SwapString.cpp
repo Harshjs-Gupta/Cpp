92af2b2e3747d31130c72b67eac09a2ca8bcabb6
Harsh Gupta <harshgupta88911@gmail.com>
2023-12-03 10:56:40 +0530

    Swap String

---
+++ String/SwapString.cpp
@@ -0,0 +1,13 @@
+//write a program to swap the string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+   string str1="Programming ";
+   string str2="Language";
+   str1.swap(str2);
+   cout<<str1<<endl;
+   cout<<str2<<endl;
+return 0;
+}
